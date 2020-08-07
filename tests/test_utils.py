ec3799e9140d5db7faa60b6f3dafe5390851c714
Sarah Gibson <sgibson@turing.ac.uk>
2020-08-07 16:03:17 +0100

    Test funcs in utils.py

---
+++ tests/test_utils.py
@@ -0,0 +1,22 @@
+import pytest
+from unittest.mock import call, patch
+
+from az_audit.utils import login
+
+
+def test_login():
+    mock_password = patch("az_audit.utils.getpass.getpass", return_value="this_is_a_password")
+    mock_input = patch("az_audit.utils.input")
+    mock_input.side_effect = ["some-email@email.com", "test_sub"]
+    mock_run = patch("az_audit.utils.run_cmd")
+    mock_run.side_effect = [
+        {"returncode": 0}, {"returncode": 0}
+    ]
+
+    with mock_input as mock1, mock_password as mock2, mock_run as mock3:
+        login()
+
+        assert mock1.call_count == 2
+        mock2.assert_called_once()
+        assert mock2.return_value == "this_is_a_password"
+        assert mock3.call_count == 2
