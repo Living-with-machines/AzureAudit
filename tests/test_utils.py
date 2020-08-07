0dc7ceadae18d62bebf396bdf6cc4e01659e4a5a
Sarah Gibson <sgibson@turing.ac.uk>
2020-08-07 16:44:41 +0100

    Run formatter

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,13 +5,13 @@ from az_audit.utils import login
 
 
 def test_login():
-    mock_password = patch("az_audit.utils.getpass.getpass", return_value="this_is_a_password")
+    mock_password = patch(
+        "az_audit.utils.getpass.getpass", return_value="this_is_a_password"
+    )
     mock_input = patch("az_audit.utils.input")
     mock_input.side_effect = ["some-email@email.com", "test_sub"]
     mock_run = patch("az_audit.utils.run_cmd")
-    mock_run.side_effect = [
-        {"returncode": 0}, {"returncode": 0}
-    ]
+    mock_run.side_effect = [{"returncode": 0}, {"returncode": 0}]
 
     with mock_input as mock1, mock_password as mock2, mock_run as mock3:
         login()
