8a4d1f26a141e2f46e861b8e75f1dd158f0d0c63
Sarah Gibson <sgibson@turing.ac.uk>
2020-08-12 13:26:47 +0100

    Test get_objects func

--- az_audit/__init__.py
+++ az_audit/__init__.py
@@ -1,5 +1,5 @@
 from .helper_functions import construct_cmd, create_timestamp, run_cmd
 
-from .storage import generate_sas_token, list_all_storage_accounts
+from .storage import generate_sas_token, get_objects, list_all_storage_accounts
 
 from .utils import login
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import call, patch
 
-from az_audit.storage import generate_sas_token, list_all_storage_accounts
+from az_audit.storage import generate_sas_token, get_objects, list_all_storage_accounts
 
 
 @patch(
@@ -40,27 +40,34 @@ def test_generate_sas_token():
         "table": None,
     }
 
-    mock_time = patch("az_audit.storage.create_timestamp", return_value="2020-0807T16:45Z")
-    mock_run = patch("az_audit.storage.run_cmd", return_value={"returncode": 0, "output": "this_is_a_sas_token"})
+    mock_time = patch(
+        "az_audit.storage.create_timestamp", return_value="2020-0807T16:45Z"
+    )
+    mock_run = patch(
+        "az_audit.storage.run_cmd",
+        return_value={"returncode": 0, "output": "this_is_a_sas_token"},
+    )
 
-    expected_call = call([
-        "az",
-        "storage",
-        "account",
-        "generate-sas",
-        "--expiry",
-        "2020-0807T16:45Z",
-        "--permissions",
-        "l",  # List permissions only
-        "--resource-types",
-        "cos",  # Permissions for Container, Object and Service types
-        "--services",
-        "bf",
-        "--ids",
-        "test_id",
-        "-o",
-        "tsv",
-    ])
+    expected_call = call(
+        [
+            "az",
+            "storage",
+            "account",
+            "generate-sas",
+            "--expiry",
+            "2020-0807T16:45Z",
+            "--permissions",
+            "l",  # List permissions only
+            "--resource-types",
+            "cos",  # Permissions for Container, Object and Service types
+            "--services",
+            "bf",
+            "--ids",
+            "test_id",
+            "-o",
+            "tsv",
+        ]
+    )
 
     with mock_time as mock1, mock_run as mock2:
         output = generate_sas_token(test_account)
@@ -69,3 +76,54 @@ def test_generate_sas_token():
         mock2.assert_called_once()
         assert mock2.call_args == expected_call
         assert output == "this_is_a_sas_token"
+
+
+@patch("az_audit.storage.run_cmd", return_value={"returncode": 0, "output": ""})
+def test_get_objects_short(mock):
+    account_name = "test_account"
+    sas_token = "this_is_a_sas_token"
+
+    output = get_objects(account_name, sas_token, "container")
+
+    mock.assert_called_once()
+    assert output is None
+
+
+@patch("az_audit.storage.run_cmd")
+def test_get_objects_long_tabs(mock):
+    account_name = "test_account"
+    sas_token = "this_is_a_sas_token"
+
+    mock.side_effect = [
+        {"returncode": 0, "output": "container1\tcontainer2"},
+        {"returncode": 0, "output": "blob1\tblob2"},
+        {"returncode": 0, "output": "blob3\tblob4"},
+    ]
+
+    output = get_objects(account_name, sas_token, "container")
+
+    assert mock.call_count == 3
+    assert output == {
+        "container1": ["blob1", "blob2"],
+        "container2": ["blob3", "blob4"],
+    }
+
+
+@patch("az_audit.storage.run_cmd")
+def test_get_objects_long_newlines(mock):
+    account_name = "test_account"
+    sas_token = "this_is_a_sas_token"
+
+    mock.side_effect = [
+        {"returncode": 0, "output": "share1\nshare2"},
+        {"returncode": 0, "output": "file1\nfile2"},
+        {"returncode": 0, "output": "file3"},
+    ]
+
+    output = get_objects(account_name, sas_token, "share")
+
+    assert mock.call_count == 3
+    assert output == {
+        "share1": ["file1", "file2"],
+        "share2": ["file3"],
+    }
