52db94f54993bd984375114e057f67cbc44f153d
Sarah Gibson <sgibson@turing.ac.uk>
2020-08-12 14:27:01 +0100

    Update login func with subscripton arg

--- az_audit/utils.py
+++ az_audit/utils.py
@@ -3,9 +3,13 @@ import getpass
 from .helper_functions import run_cmd
 
 
-def login() -> None:
-    """Login to Azure account and set active subscription."""
-    print("Please prove the login credentials to your Turing Azure account")
+def login(subscription: str) -> None:
+    """Login to Azure account and set active subscription.
+
+    Args:
+        subscription (str): The Azure subscription to audit
+    """
+    print("Please provide the login credentials to your Azure account")
 
     if "EMAIL" in os.environ:
         email = os.environ["EMAIL"]
@@ -17,8 +21,6 @@ def login() -> None:
     login_cmd = ["az", "login", "-u", email, "-p", password, "-o", "none"]
     run_cmd(login_cmd)
     print("Successfully logged into Azure")
-
-    subscription = input("Please provide a subscription name or hex-string: ")
 
     sub_cmd = ["az", "account", "set", "-s", f"{subscription}"]
     run_cmd(sub_cmd)
